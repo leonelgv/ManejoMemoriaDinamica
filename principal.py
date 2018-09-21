db2d5580cab49c92c5f342473b4edd2b4055be34
Leonel Gonzalez Vidales <leonelgv@gmail.com>
2018-09-21 10:28:32 -0500

    se agrego contenido al archivo principal

---
+++ principal.py
@@ -0,0 +1,7 @@
+
+promedio = 9.67
+otralista = []
+supermercado = ['fruta','agua', 'refresco', 'pan', 'pastel']
+estructuradedatos = ['agustin pimentel', 'francisco pineda', 'enrique bello']
+precios = [12, 34, 45, 47]
+porcentajes = [.34, .56, .12, 1.2]
